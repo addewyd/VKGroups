8cd0b5bf63cbab75ac6c114f53347352f68ea9f0
HeaTTheatR <gorodage@gmail.com>
2018-02-06 20:36:57 +0200

    Add unittest

---
+++ test/test_app.py
@@ -0,0 +1,14 @@
+'''
+This file is responsible for testing Apps inside the VKGroups project.
+'''
+
+import unittest
+
+
+class AppTest(unittest.TestCase):
+
+    def test_vkgroups_app(self):
+        from vkgroups import VKGroups
+        vkgroups = VKGroups()
+        #d.bind(started=partial(d.stop))
+        vkgroups.run()
