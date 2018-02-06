6ec68b9f2dbbbcdea5e62ab0b288dc77597a9905
HeaTTheatR <gorodage@gmail.com>
2018-02-06 21:10:44 +0200

    Remove comment

--- test/test_app.py
+++ test/test_app.py
@@ -10,5 +10,4 @@ class AppTest(unittest.TestCase):
     def test_vkgroups_app(self):
         from vkgroups import VKGroups
         vkgroups = VKGroups()
-        #d.bind(started=partial(d.stop))
         vkgroups.run()
