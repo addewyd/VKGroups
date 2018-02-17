3c3c5caeafc4c8d882acee788ffdeaad25447683
HeaTTheatR <gorodage@gmail.com>
2018-02-17 23:35:59 +0200

    Modified

--- test/test_app.py
+++ test/test_app.py
@@ -2,8 +2,6 @@
 This file is responsible for testing Apps inside the VKGroups project.
 '''
 
-import os
-import sys
 import unittest
 
 from kivy.clock import Clock
@@ -12,8 +10,7 @@ from kivy.clock import Clock
 class AppTest(unittest.TestCase):
 
     def test_creator_app(self):
-        print(os.path.abspath(sys.argv[0])[0])
         from vkgroups import VKGroups
-        test = VKGroups
+        test = VKGroups()
         Clock.schedule_once(test.stop, 1)
         test.run()
